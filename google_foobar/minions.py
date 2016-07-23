f55f0580be62b955369d2f6c2429a8215a527dc5
Anirudh <anirudhx5@gmail.com>
2016-07-23 08:37:57 +0100

    don't mind the map

--- google_foobar/minions.py
+++ google_foobar/minions.py
@@ -15,3 +15,64 @@ def answer(t, n):
     return rec(t, 1)
 
 print(answer(3, 2))
+
+
+# from itertools import permutations
+#
+# def answer(t, n):
+#
+#     def rec(N, n, T, t, seq):
+#         if n > N or t > T or n < 1:
+#             return seq
+#
+#         if n == N:
+#             if t == T:
+#                 return seq
+#             else:
+#                 return rec(N, n, T, t+1, seq + ['S'])
+#
+#         if n == 1:
+#             return rec(N, n+1, T, t+1, seq + ['R']) or rec(N, n, T, t+1, seq + ['S'])
+#
+#         return rec(N, n+1, T, t+1, seq + ['R']) or rec(N, n, T, t+1, seq + ['S']) or rec(N, n-1, T, t+1, seq + ['L'])
+#
+#     seq = rec(n, 1, t, 0, [])
+#     return count_iterable(permutations(seq)) % 123454321
+#
+# def count_iterable(i):
+#     dp = {}
+#     for j in i:
+#         if str(j) not in str(dp):
+#             dp[str(j)] = 1
+#     return len(dp) % 123454321
+
+
+# def answer(t, n):
+#
+#     dp = {}
+#
+#     def rec(N, n, T, t, seq):
+#         if n > N or t > T or n < 1:
+#             return
+#
+#         if n == N:
+#             if t == T:
+#                 dp[str(seq)] = 1
+#                 return
+#             else:
+#                 rec(N, n, T, t+1, seq + ['S'])
+#                 return
+#
+#         if n == 1:
+#             rec(N, n+1, T, t+1, seq + ['R'])
+#             rec(N, n, T, t+1, seq + ['S'])
+#             return
+#
+#         rec(N, n+1, T, t+1, seq + ['R'])
+#         rec(N, n, T, t+1, seq + ['S'])
+#         rec(N, n-1, T, t+1, seq + ['L'])
+#
+#         return
+#
+#     rec(n, 1, t, 0, [])
+#     return len(dp) % 123454321
--- hackerrank/equal.py
+++ hackerrank/equal.py
@@ -1,2 +1,21 @@
+def give(arr, i, choc):
+    for j in range(len(arr)):
+        if j is not i:
+            arr[j] += choc
+    return arr
+
+def check(arr):
+    return all(map(lambda m: m is arr[0], arr))
+
 def answer(arr):
-    
+    if check(arr):
+        return 0
+    ret = 1000
+    for i in range(len(arr)):
+        temp = min(answer(give(arr, i, 1)),
+                answer(give(arr, i, 2)),
+                answer(give(arr, i, 5)))
+        ret = min(temp, ret)
+    return ret
+
+print(answer([2,2,3]))

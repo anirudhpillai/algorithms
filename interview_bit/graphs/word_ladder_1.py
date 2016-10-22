0852c578bede396f48a19c6a4bafac0ed858b98d
Anirudh <anirudhx5@gmail.com>
2016-10-22 22:44:44 +0100

    graph problems

---
+++ interview_bit/graphs/word_ladder_1.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def ladderLength(start, end, dictV):
+    dictV.append(end)
+    def valid(one, two):
+        one, two = list(one), list(two)
+        tot = 0
+        for i in range(len(one)):
+            if one[i] != two[i]:
+                tot += 1
+                if tot > 1:
+                    return False
+
+        return True
+
+    queue = deque()
+    seen = []
+    queue.append((start, 1))
+    while queue:
+        # print(queue)
+        temp, curr = queue.pop()
+        if temp == end:
+            return curr
+
+        if temp in seen:
+            continue
+        else:
+            seen.append(temp)
+
+        queue.extendleft((i, curr+1) for i in dictV if valid(i, temp))
+
+    return 0
+
+print(ladderLength("hit", "cog", ["hot","dot","dog","lot","log"]))

6ed07fb8ada2c2e29be679156111237e02790e1b
Anirudh <anirudhx5@gmail.com>
2017-02-08 20:31:41 +0000

    first missing positive

--- leetcode/first_missing_positive.py
+++ leetcode/first_missing_positive.py
@@ -18,4 +18,24 @@ def first_missing_positive(nums):
 
     return len(nums)+1
 
+
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def swap(i, j):
+            nums[i], nums[j] = nums[j], nums[i]
+
+        for i in range(len(nums)):
+            while 0 < nums[i] < len(nums) and nums[i] != nums[nums[i]-1]:
+                swap(i, nums[i]-1)
+
+        for i in range(len(nums)):
+            if nums[i] != i+1:
+                return i+1
+
+        return len(nums)+1
+
 print(first_missing_positive([3, 4, -1, 13]))

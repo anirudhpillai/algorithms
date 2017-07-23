28f95d574003955a513301ce0ce3c69c17469c66
Anirudh <anirudhx5@gmail.com>
2017-07-23 18:40:40 +0100

    smallest range

---
+++ leetcode/smallest_range.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def smallestRange(self, nums):
+        """
+        :type nums: List[List[int]]
+        :rtype: List[int]
+        """
+        ans = -1e9, 1e9
+
+        for right in sorted(set(x for row in nums for x in row))[::-1]:
+            left = 1e9
+
+            for row in nums:
+                while row and row[-1] > right:
+                    row.pop()
+
+                if not row:
+                    return ans
+
+                left = min(left, row[-1])
+
+            if right - left <= ans[1] - ans[0]:
+                ans = left, right
+
+        return ans

c99c546d6f34474b426881ad7ba0dbdd73d12207
Anirudh <anirudhx5@gmail.com>
2017-08-03 21:32:34 +0100

    unique paths

--- leetcode/unique_paths.py
+++ leetcode/unique_paths.py
@@ -1,24 +1,14 @@
-def unique_paths(m, n):
-    if m == 0 or n == 0:
-        return 0
-    if m == 1 or n == 1:
-        return 1
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        dp = [[1]*n for _ in range(m)]
 
-    dp = [[0 for i in range(n)] for i in range(m)]
+        for row in range(1, m):
+            for col in range(1, n):
+                dp[row][col] = dp[row-1][col] + dp[row][col-1]
 
-    for i in range(m):
-        dp[i][0] = 1
-
-    for i in range(n):
-        dp[0][i] = 1
-
-    for i in range(1, m):
-        for j in range(1, n):
-            dp[i][j] = dp[i-1][j] + dp[i][j-1]
-
-    for i in dp:
-        print(i)
-
-    return dp[m-1][n-1]
-
-print(unique_paths(2, 2))
+        return dp[-1][-1]
--- leetcode/unique_paths_2.py
+++ leetcode/unique_paths_2.py
@@ -4,18 +4,19 @@ class Solution(object):
         :type obstacleGrid: List[List[int]]
         :rtype: int
         """
-        dp = [[0 for _ in range(len(obstacleGrid[0]))] for _ in range(len(obstacleGrid))]
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0]*n for _ in range(m)]
 
-        for i in range(len(obstacleGrid)):
-            for j in range(len(obstacleGrid[0])):
-                if obstacleGrid[i][j] != 1:
-                    if i == 0 and j == 0:
-                        dp[i][j] = 1
-                    elif i == 0:
-                        dp[i][j] = dp[i][j-1]
-                    elif j == 0:
-                        dp[i][j] = dp[i-1][j]
+        for row in range(m):
+            for col in range(n):
+                if obstacleGrid[row][col] != 1:
+                    if row == 0 and col == 0:
+                        dp[row][col] = 1
+                    elif row == 0:
+                        dp[row][col] = dp[row][col-1]
+                    elif col == 0:
+                        dp[row][col] = dp[row-1][col]
                     else:
-                        dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                        dp[row][col] = dp[row-1][col] + dp[row][col-1]
 
         return dp[-1][-1]

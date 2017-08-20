7d16a71d5ea3782658a01243718e07d60b9237c9
Anirudh <anirudhx5@gmail.com>
2017-08-20 21:16:03 +0100

    loving bit manipulation

--- leetcode/repeated_dna_sequences.py
+++ leetcode/repeated_dna_sequences.py
@@ -1,17 +1,25 @@
 class Solution(object):
     def findRepeatedDnaSequences(self, s):
-        """
-        :type s: str
-        :rtype: List[str]
-        """
-        dp = {}
-        result = []
-        for i in range(len(s)):
-            curr = s[i:i+10]
-            if curr in dp:
-                if dp[curr]:
-                    result.append(curr)
-                    dp[curr] = False
+        """Using Bit Manipulation
+        memo = {}
+        result = set()
+
+        for i in range(len(s)-9):
+            mask = 0
+            for c in s[i:i+10]:
+                mask <<= 5
+                mask |= ord(c) - ord('A')
+
+            if mask in memo:
+                result.add(s[i:i+10])
             else:
-                dp[curr] = True
-        return result
+                memo[mask] = True
+
+        return list(result)
+        """
+        memo = {}
+
+        for i in range(len(s)-9):
+            memo[s[i:i+10]] = memo.get(s[i:i+10], 0) + 1
+
+        return [k for k, v in memo.items() if v > 1]

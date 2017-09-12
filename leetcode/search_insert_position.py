500917d430f9d206afbfe6bd5e96d811f8addc06
Anirudh <anirudhx5@gmail.com>
2017-09-12 19:53:19 +0100

    search insert position

--- leetcode/search_insert_position.py
+++ leetcode/search_insert_position.py
@@ -1,17 +1,19 @@
-def search_insert(nums, target):
-        low, high = 0, len(nums)-1
-        mid = int((high + low)/2)
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        low, high = 0, len(nums) - 1
 
         while low <= high:
-            mid = int((high + low)/2)
+            mid = (low + high) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
-                low = mid+1
+                low = mid + 1
             else:
-                high = mid-1
+                high = mid - 1
 
-        if target > nums[mid]:
-            return mid + 1
-        else:
-            return mid
+        return low

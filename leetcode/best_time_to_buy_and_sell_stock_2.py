b5323e686f18f47bc68ffee03dfe2737ffcf8988
Anirudh <anirudhx5@gmail.com>
2017-09-21 21:22:35 +0100

    one liner

---
+++ leetcode/best_time_to_buy_and_sell_stock_2.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        return sum(max(prices[i] - prices[i-1], 0) for i in range(1, len(prices)))
